92b8aa200b7c9340a0e3c36c114247acad8ab160
justwana-fly <nicolaibarbarasa@gmail.com>
2024-04-17 12:25:37 +0200

    scheletro fatto

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+Header
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent',
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+Main
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MainComponent',
+        
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
